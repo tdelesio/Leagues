6fd6e654fcce610a9533a55085b52f10fe9ad716
Tim Delesio <tdelesio@gmail.com>
2014-08-25 08:51:07 -0400

    fix merge issues by greg

--- ws-myp/src/main/webapp/_/js/util.js
+++ ws-myp/src/main/webapp/_/js/util.js
@@ -1,4 +1,6 @@
-<<<<<<< HEAD
+var server = "http://localhost:8080/ws/rest";
+//var server = "http://makeurpicks.com/ws/rest";
+
 $.fn.serializeObject = function() {
 
 	// convert the form to an array
@@ -58,8 +60,7 @@ function getURLParameter(name) {
 	    );
 	}
 
-//var server = "http://localhost:8080/ws/rest";
-var server = "http://makeurpicks.com/ws/rest";
+
 
 function getUrl(context)
 {
@@ -78,75 +79,5 @@ function getUrl(context)
 		url = server + context;
 	}
 	return url;
-=======
-$.fn.serializeObject = function() {
-
-	// convert the form to an array
-	var a = this.serializeArray();
-
-	var arr = {};
-	// loop through all the form elements
-	$.each(a, function() {
-		// check to see if there is a . in the name
-		if (this.name.indexOf(".") != -1) {
-			// there is a doc, so we need to create some objects
-
-			// convert the name into an array
-			var b = this.name.split('.');
-
-			// pull off the version item in the array
-			var name = b.shift();
-
-			// recurisvly call the method and repeat
-			arr[name] = parseName({}, b, this.value);
-		} else {
-			// set the object value
-			arr[this.name] = this.value;
-		}
-
-	});
-
-	arr = JSON.stringify(arr);
-	console.log(arr);
-	return arr;
-};
-
-function parseName(r, name, value) {
-
-	if (name.length == 1) {
-		// no ., so set the name to the value
-		var key = name.shift();
-
-		// create a new object and set the value
-		var arr = {};
-		arr[key] = value;
-
-		// return the newly created object
-		return arr;
-	} else {
-		var key = name.shift();
-		var v = parseName(r, name, value);
-		r[key] = v;
-
-		return r;
-	}
 }
 
-
-var server = "http://makeurpicks.com/ws/rest";
-
-function getUrl(context)
-{
-	var url="";
-	if (localStorage['tgt'])
-	{
-		tgt = localStorage['tgt'];
-		url = server + context +'?tgt='+tgt;
-	}
-	else
-	{
-		url = server + context;
-	}
-	return url;
->>>>>>> refs/remotes/origin/master
-}
